617342f829878794e1ccbceb3694b99656591354
MasterinoSplinterino <kashgisya@gmail.com>
2025-11-25 19:45:54 +0300

    Move API keys to .env

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
-API_TOKEN = "8216575053:AAEZWPgZP8Owoc5zlkfNr7UKjTkh3I6H75w"
-API_ID = 30404126  # Replace with your API_ID
-API_HASH = "ff49bbdee319a44eb1e5de29e79d0fe7"  # Replace with your API_HASH
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_TOKEN = os.getenv('API_TOKEN')
+API_ID = int(os.getenv('API_ID', 0))
+API_HASH = os.getenv('API_HASH')
+
